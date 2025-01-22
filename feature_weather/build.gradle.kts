66c1278d189d5e3ed6aee04c8cfcebbd55484829
Jyo561 <jyotiradityakuanar34@gmail.com>
2025-01-23 01:01:05 +0530

    changes

--- feature_weather/build.gradle.kts
+++ feature_weather/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":base"))
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.core.ktx)
